c65c4023b47afeb35a1499ba4f89fe1f09f55e80
mpmorley <mmorley@pennmedicine.upenn.edu>
2019-02-01 12:12:38 -0500

    Added custom color allette

--- SeuratV3_Extras.R
+++ SeuratV3_Extras.R
@@ -1,6 +1,12 @@
 require(broom)
 #library(slingshot)
 
+cpallette=c("#64B2CE", "#DA5724", "#74D944", "#CE50CA", "#C0717C", "#CBD588", "#5F7FC7", 
+            "#673770", "#D3D93E", "#8569D5", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", 
+            "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", 
+            "#8A7C64", "#599861")
+
+
 processExper <- function(dir,name,org='mouse',files,ccscale=F){
   try(if(length(files)==0) stop("No files"))
  
